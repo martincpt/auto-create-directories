417df3945bf1ecc6679f3f9e869a0872167e3d1e
Martin Trapp <martincpt@gmail.com>
2022-09-06 10:41:31 +0200

    Update test_auto_create_dirs.py

--- tests/test_auto_create_dirs.py
+++ tests/test_auto_create_dirs.py
@@ -1,7 +1,6 @@
+import unittest
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import unittest
 
 from auto_create_directories import AutoCreateDirectories, auto_create_dirs
 
